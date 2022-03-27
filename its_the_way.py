7442e1c61291f893d8bc00ce0aeaf6a3a7af4d6a
Nechama Galili <ntgalili@gmail.com>
2022-03-27 13:02:08 +0300

    fix its_the_way

--- its_the_way.py
+++ its_the_way.py
@@ -2,21 +2,21 @@
 import os
 
 
-def file_start(path :str,start:str):
+def files_start_with(path :str, prefix:str):
     """
-    the function get a path of a directory and find the files in the directory that start with start
-    :param path: the path of the directory
-    :param start: the str that the file's names start with
-    :return: list of files
+        the function get a path of a directory and find the files in the directory that start with start
+        :param path: the path of the directory
+        :param prefix: the str that the file's names start with
+        :return: list of files
     """
     list_of_files = os.listdir(path) # get the list of the files in the directory
-    starts_files = [file for file in list_of_files if file.startswith(start)] # get the list of the files that start with start
+    starts_files = [file for file in list_of_files if file.startswith(prefix)] # get the list of the files that start with start
     return starts_files
 
 
 def main():
     path = input("Enter a path of directory: ")
-    deep_file = file_start(path,'deep') # find the files in the directory that start with 'deep'
+    deep_file = files_start_with(path, 'deep') # find the files in the directory that start with 'deep'
     print(deep_file)
 
 
