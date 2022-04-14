1695aee6c614f735a529e7e46994dd13615525ef
Nechama Galili <ntgalili@gmail.com>
2022-04-15 01:17:13 +0300

    my filter

---
+++ my_filter.py
@@ -0,0 +1,12 @@
+from typing import Callable
+
+
+def my_filter(function_to_apply: Callable ,iterable: iter) -> list:
+    """
+    The function applies the function it received on the iterable and filters the True results
+    :param function_to_apply: function to apply on the iterable
+    :param iterable: Iterable used as parameters for the function
+    :return: List of returned results whose value is true
+    """
+    return [function_to_apply(item) for item in iterable if function_to_apply(item)] 
+
