fca0c8992c130c5ecf5b478da494e84b14d1a4dc
Nechama Galili <ntgalili@gmail.com>
2022-04-23 22:11:18 +0300

    tzav shaluach 2

---
+++ message_class.py
@@ -0,0 +1,77 @@
+class Message:
+    """
+    Message class. Represents a virtual message
+
+    :ivar id: id to the message.
+    :ivar sender: The sender's name.
+    :ivar body: The message content.
+
+    """
+    def __init__(self, message_id: int, message_sender: str, message_body: str):
+        self.id = message_id
+        self.sender = message_sender
+        self.body = message_body
+
+    def __str__(self):
+        return f"message id: {str(self.id)} message sender: {self.sender} \n {self.body}"
+
+    def __len__(self):
+        return len(self.body)
+
+
+class PostOffice:
+    """A Post Office class. Allows users to message each other.
+
+    :ivar id: Incremental id of the last message sent.
+    :ivar dict boxes: Users' inboxes.
+
+    :param list usernames: Users for which we should create PO Boxes.
+    """
+
+    def __init__(self, usernames):
+        self.message_id = 0
+        self.boxes = {user: [] for user in usernames}
+
+    def send_message(self, sender, recipient, message_body, urgent=False):
+        """Send a message to a recipient.
+
+        :param str sender: The message sender's username.
+        :param str recipient: The message recipient's username.
+        :param str message_body: The body of the message.
+        :param urgent: The urgency of the message.
+        :type urgent: bool, optional
+        :return: The message ID, auto incremented number.
+        :rtype: int
+        :raises KeyError: if the recipient does not exist.
+        """
+        user_box = self.boxes[recipient]
+        self.message_id = self.message_id + 1
+        message = Message(self.message_id, sender, message_body)
+        if urgent:
+            user_box.insert(0, message)
+        else:
+            user_box.append(message)
+        return self.message_id
+
+    def read_inbox(self, username: str, number_of_messages: int = None) -> list:
+        """
+        The function returns the number of messages requested from the user's mailbox.
+        :param username: The name of the user that requesting his messages.
+        :param number_of_messages: Number of messages requested.
+        :return: List of the requested messages.
+        :raises KeyError: if the username does not exist.
+        """
+        to_return = self.boxes[username][:number_of_messages]
+        del self.boxes[username][number_of_messages:]
+        return to_return
+
+    def search_inbox(self, username: str, to_search: str) -> list:
+        """
+        The function search str in user box.
+        :param username: The searcher's name.
+        :param to_search: The str to search.
+        :return: The messages that contain the string in their body or their title.
+        :raises KeyError: if the username does not exist.
+        """
+        user_box = self.boxes[username]
+        return [message for message in user_box if (to_search in str(message))]
