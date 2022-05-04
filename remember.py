b011af2b9a87f195e5129737a8a8d8d737efcd81
Nechama Galili <ntgalili@gmail.com>
2022-05-04 13:42:33 +0300

    fix remember

--- remember.py
+++ remember.py
@@ -1,15 +1,18 @@
 from PIL import Image
 
 
-def decode_image(path):
+def decode_image(path: str) -> str:
     """
-    The function decodes an image by to the position of the black pixels
+    The function decodes an image according to the position of the black pixels.
     :param path: A path of the image to decode
     :return: A decoded string
     """
     image = Image.open(path,'r')
     pixels=image.load() # load the pixels
     width,height=image.size
-    black_pixels_rows = [i for j in range(height) for i in range(width) if pixels[i,j] == 1] # find the rows of the black pixels
-    chars=[chr(pix) for pix in black_pixels_rows]  # decode the row number to char
-    return "".join(chars)  
+    black_pixels_rows = [i for j in range(width) for i in range(height) if pixels[j,i] == 1] # find the rows of the black pixels
+    return "".join([chr(pix) for pix in black_pixels_rows])  # decode the row number to char
+
+
+
+#print(decode_image("C:\\Users\\nechama\\Notebooks\\week06\\resources\\code.png"))
